339ba90d5b6a5b5fd85b0c3755b05693e6ceeefd
Nathan Steele <nsteele1031@gmail.com>
2025-12-17 08:56:44 -0500

    Adding owndership to player data

--- player.py
+++ player.py
@@ -5,18 +5,28 @@ class Player:
         if not file:
             self.level = 1
             self.items = []
+            self.owner = None
         else:
             self.load(file)
     
+    def set_owner(self, owner):
+        """
+        The value of owner should be a discord user id
+        """
+
+        self.owner = owner
+
     def load(self, file):
         with open(file, 'r') as f:
             data = json.load(f)
             self.level = data['level']
             self.items = data['items']
+            self.owner = data['owner']
         
     def save(self, file):
         with open(file, 'w') as f:
             json.dump({
                 'level': self.level,
-                'items': self.items
+                'items': self.items,
+                'owner': self.owner
             }, f)
